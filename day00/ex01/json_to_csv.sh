3b1ddbe13b0595427f74de636e12197fe162b2a4
Dacia Nicki <dnicki@ox-s1.msk.21-school.ru>
2022-03-31 21:17:30 +0300

    day00 ex00-02 done, 03 in progress

---
+++ day00/ex01/json_to_csv.sh
@@ -0,0 +1,4 @@
+#!bin/sh
+
+
+jq -r -f filter.jq < hh.json > hh.csv
---
+++ day00/ex03/test.py
@@ -0,0 +1,17 @@
+import os
+
+from itertools import combinations
+import pandas as pd
+
+def	remove_other():
+	words = ["Junior", "Middle", "Senior"]
+	all_words = []
+	for i in range(len(words) + 1):
+		
+
+
+
+
+df = pd.read_csv("../ex02/hh_sorted.csv")
+
+df["name"] = df["name"].map()
