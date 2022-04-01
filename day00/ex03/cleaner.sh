55aa7580dc5c332a0a465c05632e0b859dbb9339
Dacia Nicki <dnicki@ox-s1.msk.21-school.ru>
2022-04-01 22:17:40 +0300

    final 03

--- day00/ex03/cleaner.sh
+++ day00/ex03/cleaner.sh
@@ -1,6 +1,7 @@
 #!bin/sh
 
-curl -s https://raw.githubusercontent.com/LinearBasis/ds_piscine/master/day00/cleaner.py > script.py
+curl -s https://raw.githubusercontent.com/LinearBasis/ds_piscine/master/day00/cleaner.py > cleaner.py
 python3 -m pip install --upgrade pip > /dev/null
 pip3 install pandas > /dev/null
-python3 script.py > hh_positions.csv
+python3 cleaner.py > hh_positions.csv
+rm cleaner.py
